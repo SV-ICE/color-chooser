a828db4f48517286494e288bc830c807d07c808f
SV-ICE <hristyuk.sv@gmail.com>
2025-12-06 01:04:33 +0200

    🧩 refactor(setLock): упростить логику блокировки цвета
    Упрощена логика функции setLock для улучшения читаемости и производительности. Убрано лишнее создание объекта в map, если id цвета не совпадает. Переименована переменная lock в isLocked для большей ясности.

--- src/modules/setLock.js
+++ src/modules/setLock.js
@@ -2,15 +2,13 @@ export const setLock = (id, setColors, setIsNotify) => {
 	setColors((prevColors) => {
 		return prevColors.map((color) => {
 			if (color.id !== id) {
-				return {
-					...color,
-				}
+				return color;
 			}
-			let lock = color.locked;
-			setIsNotify(lock ? "Цвет разблокирован" : "Цвет заблокирован");
+			let isLocked = color.locked;
+			setIsNotify(isLocked ? "Цвет разблокирован" : "Цвет заблокирован");
 			return {
 				...color,
-				locked: !lock,
+				locked: !isLocked,
 			};
 		});
 	});
