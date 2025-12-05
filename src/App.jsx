3890c320e677a574eb1aa631865161bbc98d2634
SV-ICE <hristyuk.sv@gmail.com>
2025-12-05 17:22:23 +0200

    🪛 fix(ui): исправить отображение иконки кнопки
    - Исправлено отображение иконки внутри кнопки: добавлен `display: block`.
    - Убрано `setIsModalOpen("")` из `useEffect`, так как это вызывало нежелательное закрытие модального окна.

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ function App() {
     useEffect(() => {
         const notifyTimer = setTimeout(() => {
             setIsNotify("");
-            setIsModalOpen("");
         }, 1600);
 
         return () => clearTimeout(notifyTimer);
